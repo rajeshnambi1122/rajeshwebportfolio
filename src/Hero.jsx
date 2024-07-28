9ebe6f4c4491342677dc26f648ec19ce3f793079
Rajesh Nambi <rajeshnambi2016@gmail.com>
2024-07-28 13:51:17 +0530

    Adding Animations

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = ({ skillsRef, aboutRef, projectsRef, scrollToSection }) => {
     <div class="animate__animated animate__fadeInDown animate__delay-2s">
       <div className="hero">
         <img src={pfp} className="pfp" alt="Profile"></img>
-        <h1 className="animate__animated animate__tada animate__infinite">
+        <h1 className="animate__animated animate__pulse animate__infinite">
           Rajesh Nambi
         </h1>
         <p className="heroabout"> A Frontend Developer</p>
